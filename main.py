9baea804a09a13d1c5847ccdbf87ce901166ef68
Dikeen147 <ConstAnto147@yandex.ru>
2021-10-05 14:21:59 +0300

    test commit by Dikeen147

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from Executor import Executor
 
 def main():
     executor = Executor()
-    # lines = executor.read_source_file()
-    # executor.execute_seource_lines(lines)
-    # executor.try_set_categories_for_tile()
-    executor.select_other_rows()
+    lines = executor.read_source_file()
+    executor.execute_seource_lines(lines)
+    executor.try_set_categories_for_tile()
+    # executor.select_other_rows()
 
 
 if __name__ == '__main__':
